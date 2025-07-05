4fccc389d92b7b3066382855e4b5da45ca2b84f3
Alexa-2k <109705749+Alexa-2k@users.noreply.github.com>
2025-07-05 12:11:48 -0300

    mejoras en carousel

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 /* Alterna entre mostrar u ocultar los links de navegacion cuando el usuario hace click en el ícono hamburguesa */
 
-function showHide() {
-	var x = document.getElementById("myLinks");
-	if (x.style.display === "block") {
-		x.style.display = "none";
-	} else {
-		x.style.display = "block";
-	}
-}
+  function showHide() {
+    const menu = document.getElementById("myLinks");
+    const icon = document.querySelector(".topnav a.icon");
+
+    // Toggle class
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+
+// function showHide() {
+// 	var x = document.getElementById("myLinks");
+// 	if (x.style.display === "block") {
+// 		x.style.display = "none";
+// 	} else {
+// 		x.style.display = "block";
+// 	}
+// }
